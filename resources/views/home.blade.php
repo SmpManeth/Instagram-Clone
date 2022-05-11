7242966de0fd9f829f4b7d6e07ebd5f3518735bf
SmpManeth <smpmaneth@gmail.com>
2022-05-11 14:12:17 +0530

    frontend header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/278479793_734598264208578_7838242761727126818_n.jpg?stp=c0.17.206.206a_dst-jpg_p206x206&_nc_cat=105&ccb=1-6&_nc_sid=da31f3&_nc_ohc=VRewxaxlOwAAX_Q-t9f&tn=v_tenr3G61cAOBPQ&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9PQG9rWdlPpXAP7YgSugdjaxVnJ7PfdGCfpLMMaERqvQ&oe=6280E2A6" class="rounded-circle" alt="Profile Picture">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>maneeeeeez</h1>
+                <div class="d-flex">
+                    <div class="pe-5"><strong>233</strong> posts</div>
+                    <div class="pe-5"><strong>1,360</strong> followers</div>
+                    <div class="pe-5"><strong>436</strong> following</div>
                 </div>
+                <div class="pt-3">
+                    <strong>Maneth | Senior Web Developer</strong>
+                </div>
+                <div class="text-muted">
+                    Digital creator
+                </div>
+                <div>
+                    Simple AF #simple 👨‍💻 <br>
+                    . <br>
+                    21 + <br>
+                    @wordroidsolutions <br>
+                </div>
+                <div class="fw-bold">
+                    <a class="text-decoration-none" href="#">www.wordroids.com</a>
+                </div>
+
             </div>
         </div>
     </div>
