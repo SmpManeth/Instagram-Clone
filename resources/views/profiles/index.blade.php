ff7790248753737bb8c0b906c4d2973cc63968fc
SmpManeth <smpmaneth@gmail.com>
2022-05-18 09:00:54 +0530

    header dynamic

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,17 +4,17 @@
 <div class="container profile">
     <div class="row">
         <div class="col-3 pt-5 mt-1">
-            <img src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/278479793_734598264208578_7838242761727126818_n.jpg?stp=c0.17.206.206a_dst-jpg_p206x206&_nc_cat=105&ccb=1-6&_nc_sid=da31f3&_nc_ohc=VRewxaxlOwAAX_Q-t9f&tn=v_tenr3G61cAOBPQ&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9PQG9rWdlPpXAP7YgSugdjaxVnJ7PfdGCfpLMMaERqvQ&oe=6280E2A6" class="rounded-circle" alt="Profile Picture">
+            <img src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/280278152_752424762425928_1049722008687008167_n.jpg?stp=dst-jpg_p206x206&_nc_cat=102&ccb=1-6&_nc_sid=da31f3&_nc_ohc=ugyToJBWTo8AX_E8pHt&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9yHCUaFHTyS6HHsMKBv7LZDFU-jngxDOeaIYjalmaBag&oe=6289648F" class="rounded-circle" alt="Profile Picture">
         </div>
         <div class="col-9 pt-5 ps-5">
             <div>
                 <div class="d-flex justify-content-between align-items-baseline">
 
                     <h1>{{ $user->username }}</h1>
-                    <a href="#">Add New Post</a>
+                    <a href="/p/create">Add New Post</a>
                 </div>
                 <div class="d-flex">
-                    <div class="pe-5"><strong>233</strong> posts</div>
+                    <div class="pe-5"><strong>{{ $user->posts->count()}}</strong> posts</div>
                     <div class="pe-5"><strong>1,360</strong> followers</div>
                     <div class="pe-5"><strong>436</strong> following</div>
                 </div>
@@ -36,7 +36,7 @@
     <div class="row pt-5">
         @foreach($user->posts as $post)
         <div class="col-4">
-            <img class="w-100 h-100" src="/storage/{{ $post->image }}" alt="">
+            <img class="w-100" src="/storage/{{ $post->image }}" height="300" alt="">
         </div>
         @endforeach
     </div>
